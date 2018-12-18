b4ba1fb756526a2196d96e9a443db34eea3cbfc3
florin <florinlaviniu@yahoo.com>
2018-12-18 19:03:23 +0200

    dd

---
+++ jenkinsTest/jenkinsTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jenkinsTest.Startup))]
+namespace jenkinsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
