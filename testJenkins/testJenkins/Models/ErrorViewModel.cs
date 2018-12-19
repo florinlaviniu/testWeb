910a066ee9a30c5f4df1ca56da61711e99899ac7
florin <florinlaviniu@yahoo.com>
2018-12-19 16:29:12 +0200

    aaa

---
+++ testJenkins/testJenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testJenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
